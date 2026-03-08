b92e93679acc2b837e288a5e30424f75ecdc7563
Dr. Jari Laru <jari.laru@oulu.fi>
2026-03-08 19:11:04 +0200

    Remove SlideShare tables and static fallback imports

--- src/_data/slideshare.js
+++ src/_data/slideshare.js
@@ -3,68 +3,6 @@ const { readCache, writeCache } = require("./_apiCache");
 
 const CACHE_KEY = "slideshare-presentations-v1";
 const PROFILE_URL = "https://www.slideshare.net/larux";
-const FALLBACK_ROWS = [
-  {
-    id: "228552429",
-    title: "Digital Enabled Learning - Arctic Frontiers speech 2020",
-    thumbnail: "/img/slideshare/228552429.png",
-    url: "https://www.slideshare.net/slideshow/digital-enabled-learning-arctic-frontiers-speech-2020/228552429"
-  },
-  {
-    id: "149380656",
-    title: "Educational robotics in Finland 2019",
-    thumbnail: "/img/slideshare/149380656.png",
-    url: "https://www.slideshare.net/slideshow/educational-robotics-in-finland-2019/149380656"
-  },
-  {
-    id: "148128991",
-    title: "Robotiikka ja tekoäly oppimisen tukena - uhka vai mahdollisuus (itk2019)",
-    thumbnail: "/img/slideshare/148128991.png",
-    url: "https://www.slideshare.net/slideshow/robotiikka-ja-tekoily-oppimisen-tukena-uhka-vai-mahdollisuus-itk2019/148128991"
-  },
-  {
-    id: "146730753",
-    title: "Kuulumisia opetuksen ja oppimisen digitalisaatiosta: mobiilioppimista, robotiikkaa ja tekoälyä (Raksilan lukio 2019)",
-    thumbnail: "/img/slideshare/146730753.png",
-    url: "https://www.slideshare.net/slideshow/kuulumisia-opetuksen-ja-oppimisen-digitalisaatiosta-mobiilioppimista-robotiikkaa-ja-tekoily-raksilan-lukio-2019/146730753"
-  },
-  {
-    id: "140995534",
-    title: "Digitalisaatio osaksi opettajakoulutusta - case kokeileva ja kehittävä luma-opettaja",
-    thumbnail: "/img/slideshare/140995534.png",
-    url: "https://www.slideshare.net/slideshow/digitalisaatio-osaksi-opettajakoulutusta-case-kokeileva-ja-kehittv-lumaopettaja/140995534"
-  },
-  {
-    id: "137666039",
-    title: "Digitaaliset välineet opetuksessa ja oppimisessa opettajankoulutuksen kontekstissa - tasapainoilua tulevaisuuden mahdollisuuksien ja teknologian opetuskäytön arkirealismin välillä (itk2019)",
-    thumbnail: "/img/slideshare/137666039.png",
-    url: "https://www.slideshare.net/slideshow/digitaaliset-vlineet-opetuksessa-ja-oppimisessa-opettajankoulutuksen-kontekstissa-tasapainoilua-tulevaisuuden-mahdollisuuksien-ja-teknologian-opetuskytn-arkirealismin-vlill/137666039"
-  },
-  {
-    id: "137649224",
-    title: "Digitaalisen oppimisen välineitä yhdessä kehittämässä - varhainen omaksuja, uhka vai mahdollisuus tietohallinnolle?",
-    thumbnail: "/img/slideshare/137649224.png",
-    url: "https://www.slideshare.net/slideshow/digitaalisen-oppimisen-vlineit-yhdess-kehittmss-varhainen-omaksuja-uhka-vai-mahdollisuus-tietohallinnolle-137649224/137649224"
-  },
-  {
-    id: "135625316",
-    title: "Are we currently moving from the age of mobilism to age of artificial intelligence, learning analytics and robotics?",
-    thumbnail: "/img/slideshare/135625316.png",
-    url: "https://www.slideshare.net/slideshow/are-we-currently-moving-from-the-age-of-mobilism-to-age-of-artificial-intelligence-learning-analytics-and-robotics-yes-we-arehow-to-couple-emergent-technology-with-learning-and-teaching/135625316"
-  },
-  {
-    id: "129224523",
-    title: "PREDICTING FUTURE OF EDTECH 2030 v2",
-    thumbnail: "/img/slideshare/129224523.png",
-    url: "https://www.slideshare.net/slideshow/predicting-future-of-edtech-2030-v2/129224523"
-  },
-  {
-    id: "122776081",
-    title: "Varhainen omaksuja, uhka vai mahdollisuus tietohallinnolle? (2018)",
-    thumbnail: "/img/slideshare/122776081.png",
-    url: "https://www.slideshare.net/slideshow/digitaalisen-oppimisen-vlineit-yhdess-kehittmss-varhainen-omaksuja-uhka-vai-mahdollisuus-tietohallinnolle/122776081"
-  }
-];
 
 function isPresentationRow(item) {
   return (
@@ -159,7 +97,7 @@ module.exports = async function () {
       return cachedRows.slice(0, maxItems);
     }
 
-    console.warn(`SlideShare: live fetch failed, using static fallback: ${error.message}`);
-    return FALLBACK_ROWS.slice(0, maxItems);
+    console.warn(`SlideShare: live fetch failed and cache missing: ${error.message}`);
+    return [];
   }
 };
