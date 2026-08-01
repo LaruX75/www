554c86e64fdb3e39462be7e8a59f17b831428a0a
Dr. Jari Laru <jari.laru@oulu.fi>
2026-08-01 11:52:50 +0300

    Add practical page aliases

---
+++ src/_data/pageAliases.js
@@ -0,0 +1,30 @@
+module.exports = {
+  fi: [
+    { from: "/minusta/", to: "/tietoa/" },
+    { from: "/mina/", to: "/tietoa/" },
+    { from: "/minä/", to: "/tietoa/" },
+    { from: "/yhteys/", to: "/yhteystiedot/" },
+    { from: "/ota-yhteyttä/", to: "/yhteystiedot/" },
+    { from: "/julkaisuluettelo/", to: "/julkaisut/" },
+    { from: "/opinnäytteet/", to: "/opinnaytteet/" },
+    { from: "/gradut/", to: "/opinnaytteet/" },
+    { from: "/materiaalit/", to: "/esitykset/" },
+    { from: "/luennot/", to: "/esitykset/" },
+    { from: "/esitysmateriaalit/", to: "/esitykset/" },
+    { from: "/larux/", to: "/kouluttaja/" },
+    { from: "/koulutus/", to: "/kouluttaja/" },
+    { from: "/kouluttaja-jari-laru/", to: "/kouluttaja/" },
+    { from: "/media/", to: "/mediassa/" },
+    { from: "/kirjoitukset/", to: "/kynasta/" },
+    { from: "/puheet/", to: "/kynasta/" },
+    { from: "/lausunnot/", to: "/kynasta/" },
+    { from: "/mielipiteet/", to: "/kynasta/" },
+    { from: "/lautakunta/", to: "/politiikka/sivistyslautakunta/" },
+    { from: "/sivistyslautakunta/", to: "/politiikka/sivistyslautakunta/" },
+    { from: "/vaikuttavuus/", to: "/yhteiskunnallinen-vuorovaikutus/" },
+    { from: "/vuorovaikutus/", to: "/yhteiskunnallinen-vuorovaikutus/" },
+    { from: "/yvv/", to: "/yhteiskunnallinen-vuorovaikutus/" },
+    { from: "/aiheet/", to: "/teemat/" },
+    { from: "/etsi/", to: "/haku/" }
+  ]
+};
